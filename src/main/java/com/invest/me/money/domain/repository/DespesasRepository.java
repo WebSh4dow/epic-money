ab7e5c961eaa2680776b76683075e911b5a5ad8e
FireXtz <phantomrising@protonmail.com>
2024-01-20 09:56:19 -0300

    identação de arquivos

--- src/main/java/com/invest/me/money/domain/repository/DespesasRepository.java
+++ src/main/java/com/invest/me/money/domain/repository/DespesasRepository.java
@@ -4,11 +4,11 @@ import com.invest.me.money.domain.model.Despesas;
 import com.invest.me.money.domain.repository.query.DespesaQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 
 public interface DespesasRepository extends JpaRepository<Despesas,Long>, DespesaQuery {
     Despesas findByCodigo(Long codigo);
+
     @Query(value = QUERY_DESPESA_POR_TIPO_DESPESA,nativeQuery = true)
     List<Despesas> despesasPorTipo(Long codigoTipoDespesa);
 }
